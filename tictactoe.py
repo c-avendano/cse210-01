76748b01f7236b6e2bd055a64541e30d850302ef
Cristian Avendaño <cristianmrcl@gmail.com>
2022-04-28 00:06:15 +0200

    Created the tictatoe.py program. Must check afterwards for ways to improve the code by adding checks when user inputs anumber that isnt 1-9

---
+++ tictactoe.py
@@ -0,0 +1,56 @@
+'''Tuc tac toe program attempt!
+Author: Cristian Avendaño (and Bro. Manley! Seriously, I went in circles over this, making it too complicated, your solution is MUCH simpler)'''
+
+def main():
+    player = next_player("")
+    board = create_board()
+    while not (has_winner(board) or is_a_draw(board)):
+        display_board(board)
+        make_move(player, board)
+        player = next_player(player)
+    display_board(board)
+    print("Thanks for playing!") 
+
+def create_board():
+    board = []
+    for square in range(9):
+        board.append(square + 1)
+    return board
+
+def display_board(board):
+    print()
+    print(f"{board[0]}|{board[1]}|{board[2]}")
+    print('-+-+-')
+    print(f"{board[3]}|{board[4]}|{board[5]}")
+    print('-+-+-')
+    print(f"{board[6]}|{board[7]}|{board[8]}")
+    print()
+    
+def is_a_draw(board):
+    for square in range(9):
+        if board[square] != "X" and board[square] != "O":
+            return False
+    return True 
+    
+def has_winner(board):
+    return (board[0] == board[1] == board[2] or
+            board[3] == board[4] == board[5] or
+            board[6] == board[7] == board[8] or
+            board[0] == board[3] == board[6] or
+            board[1] == board[4] == board[7] or
+            board[2] == board[5] == board[8] or
+            board[0] == board[4] == board[8] or
+            board[2] == board[4] == board[6])
+
+def make_move(player, board):
+    square = int(input(f"{player}'s turn to choose a square (1-9): "))
+    board[square - 1] = player
+
+def next_player(current):
+    if current == "" or current == "O":
+        return "X"
+    elif current == "X":
+        return "O"
+
+if __name__ == "__main__":
+    main()
